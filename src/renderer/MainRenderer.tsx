12e20da6aa7e2efe68fccb0cfd7557f73b2d8d09
Oliver Schwendener <oliver.schwendener@hotmail.com>
2022-09-12 08:18:58 +0200

    Simplified code

--- src/renderer/MainRenderer.tsx
+++ src/renderer/MainRenderer.tsx
@@ -4,6 +4,5 @@ import { Main } from "./Components/MainWindow/Main";
 const rootElement = document.getElementById("app");
 
 if (rootElement) {
-    const root = createRoot(rootElement);
-    root.render(<Main />);
+    createRoot(rootElement).render(<Main />);
 }
