b9930dd9ab316cc69f8c46bfe439a7278170d1f9
Oliver Schwendener <oliver.schwendener@hotmail.com>
2022-09-27 22:39:53 +0200

    Added moq.ts for mocking objects in tests

--- src/main/Core/SearchEngine.test.ts
+++ src/main/Core/SearchEngine.test.ts
@@ -1,131 +1,155 @@
+import { IMock, Mock, Times } from "moq.ts";
 import { join } from "path";
-import { TestLogger } from "../../common/Logger/TestLogger";
-import { SearchEngineSettings } from "../../common/Settings/SearchEngineSettings";
+import { Logger } from "../../common/Logger/Logger";
 import { SearchResultItemDummy } from "../../common/SearchResult/SearchResultItemDummy";
-import { DummySearchPlugin } from "../Plugins/DummySearchPlugin/DummySearchPlugin";
+import { SearchEngineSettings } from "../../common/Settings/SearchEngineSettings";
+import { SearchPlugin } from "../Plugins/SearchPlugin";
 import { FileSystemUtility } from "../Utilities/FileSystemUtility";
 import { DummySearchable } from "./DummySearchable";
 import { Searchable } from "./Searchable";
 import { SearchEngine } from "./SearchEngine";
 
 describe(SearchEngine, () => {
+    let logger: IMock<Logger>;
+    let searchPlugin: IMock<SearchPlugin<unknown>>;
+
     const tempFolderPath = join(__dirname, "temp");
+
     const searchEngineSettings: SearchEngineSettings = {
         automaticRescanEnabled: false,
         automaticRescanIntervalInSeconds: 0,
         threshold: 0.4,
     };
-    const logger = new TestLogger();
 
-    beforeEach(async () => await FileSystemUtility.createFolderIfDoesntExist(tempFolderPath));
+    const searchables: Searchable[] = [
+        new DummySearchable(SearchResultItemDummy.withName("Search Result Item 1")),
+        new DummySearchable(SearchResultItemDummy.withName("Search Result Item 2")),
+        new DummySearchable(SearchResultItemDummy.withName("Search Result Item 3")),
+        new DummySearchable(SearchResultItemDummy.withName("Search Result Item 4")),
+    ];
+
+    const setUpLoggerMock = (): IMock<Logger> =>
+        new Mock<Logger>()
+            .setup((instance) => instance.debug("Debug"))
+            .returns()
+            .setup((instance) => instance.info("Info"))
+            .returns()
+            .setup((instance) => instance.warning("Warning"))
+            .returns()
+            .setup((instance) => instance.error("Error"))
+            .returns();
+
+    const setUpSearchPluginMock = (): IMock<SearchPlugin<unknown>> =>
+        new Mock<SearchPlugin<unknown>>()
+            .setup((instance) => instance.createTemporaryFolder())
+            .returns(Promise.resolve())
+            .setup((instance) => instance.createSettingsFileIfNotExists())
+            .returns(Promise.resolve())
+            .setup((instance) => instance.getTemporaryFolderPath())
+            .returns(tempFolderPath)
+            .setup((instance) => instance.rescan())
+            .returns(Promise.resolve())
+            .setup((instance) => instance.getAllSearchables())
+            .returns(searchables);
+
+    const verifyLoggerLoggedInfo = (message: string, times?: Times) =>
+        logger.verify((instance) => instance.info(message), times);
+
+    beforeEach(async () => {
+        await FileSystemUtility.createFolderIfDoesntExist(tempFolderPath);
+        logger = setUpLoggerMock();
+        searchPlugin = setUpSearchPluginMock();
+    });
+
     afterEach(async () => await FileSystemUtility.deleteFolderRecursively(tempFolderPath));
 
     it("should create plugin temp folders and trigger a rescan on instantiation", async () => {
-        const onRescan = jest.fn(() => Promise.resolve());
-        const dummySearchPlugin = new DummySearchPlugin(tempFolderPath, undefined, onRescan, undefined);
-        const searchEngine = new SearchEngine(searchEngineSettings, [dummySearchPlugin], logger);
+        const searchEngine = new SearchEngine(searchEngineSettings, [searchPlugin.object()], logger.object());
         await searchEngine.initialize();
-        const pluginFolderExists = await FileSystemUtility.pathExists(dummySearchPlugin.getTemporaryFolderPath());
+        const pluginFolderExists = await FileSystemUtility.pathExists(searchPlugin.object().getTemporaryFolderPath());
+
         expect(pluginFolderExists).toBe(true);
-        expect(onRescan.mock.calls.length).toBe(1);
+        verifyLoggerLoggedInfo("Starting rescan", Times.Once());
+        verifyLoggerLoggedInfo("Successfully rescanned", Times.Once());
     });
 
     describe(SearchEngine.prototype.search, () => {
-        const searchables: Searchable[] = [
-            new DummySearchable(SearchResultItemDummy.withName("Search Result Item 1")),
-            new DummySearchable(SearchResultItemDummy.withName("Search Result Item 2")),
-            new DummySearchable(SearchResultItemDummy.withName("Search Result Item 3")),
-            new DummySearchable(SearchResultItemDummy.withName("Search Result Item 4")),
-        ];
-
         it("should return an empty array if search engine is not initialized yet", async () => {
-            const onGetAllSearchables = jest.fn(() => searchables);
-            const searchEngine = new SearchEngine(
-                searchEngineSettings,
-                [new DummySearchPlugin(tempFolderPath, onGetAllSearchables)],
-                logger
-            );
+            const searchEngine = new SearchEngine(searchEngineSettings, [searchPlugin.object()], logger.object());
+
             expect(searchEngine.search("item").length).toBe(0);
             await searchEngine.initialize();
             expect(searchEngine.search("item").length).toBe(searchables.length);
+
+            verifyLoggerLoggedInfo("Starting rescan", Times.Once());
         });
 
         it("should return an empty array if the search term is an empty string", async () => {
-            const onGetAllSearchables = jest.fn(() => searchables);
-            const searchEngine = new SearchEngine(
-                searchEngineSettings,
-                [new DummySearchPlugin(tempFolderPath, onGetAllSearchables)],
-                logger
-            );
+            const searchEngine = new SearchEngine(searchEngineSettings, [searchPlugin.object()], logger.object());
+
             await searchEngine.initialize();
             const actual = searchEngine.search("");
-            expect(onGetAllSearchables.mock.calls.length).toBe(0);
+
             expect(actual.length).toBe(0);
+            verifyLoggerLoggedInfo("Staring rescan", Times.Never());
         });
 
         it("should return all items that match the search term", async () => {
-            const onGetAllSearchables = jest.fn(() => searchables);
-            const searchEngine = new SearchEngine(
-                searchEngineSettings,
-                [new DummySearchPlugin(tempFolderPath, onGetAllSearchables)],
-                logger
-            );
+            const searchEngine = new SearchEngine(searchEngineSettings, [searchPlugin.object()], logger.object());
+
             await searchEngine.initialize();
             const actual = searchEngine.search("Search Result Item");
+
             expect(actual).toEqual(searchables.map((searchable) => searchable.toSearchResultItem()));
-            expect(onGetAllSearchables.mock.calls.length).toBe(1);
+            verifyLoggerLoggedInfo("Starting rescan", Times.Once());
         });
 
         it("should be case insensitive", async () => {
-            const onGetAllSearchables = jest.fn(() => searchables);
-            const searchEngine = new SearchEngine(
-                searchEngineSettings,
-                [new DummySearchPlugin(tempFolderPath, onGetAllSearchables)],
-                logger
-            );
+            const searchEngine = new SearchEngine(searchEngineSettings, [searchPlugin.object()], logger.object());
+
             await searchEngine.initialize();
             const actual = searchEngine.search("search result item");
+
             expect(actual).toEqual(searchables.map((searchable) => searchable.toSearchResultItem()));
-            expect(onGetAllSearchables.mock.calls.length).toBe(1);
+            verifyLoggerLoggedInfo("Starting rescan", Times.Once());
         });
 
         it("should return an empty array if the search term does not match any of the items", async () => {
-            const onGetAllSearchables = jest.fn(() => searchables);
-            const searchEngine = new SearchEngine(
-                searchEngineSettings,
-                [new DummySearchPlugin(tempFolderPath, onGetAllSearchables)],
-                logger
-            );
+            const searchEngine = new SearchEngine(searchEngineSettings, [searchPlugin.object()], logger.object());
+
             await searchEngine.initialize();
             const actual = searchEngine.search("whatever");
+
             expect(actual).toEqual([]);
-            expect(onGetAllSearchables.mock.calls.length).toBe(1);
+            verifyLoggerLoggedInfo("Starting rescan", Times.Once());
         });
 
         it("should support fuzzy search if threshold is high enough", async () => {
-            const onGetAllSearchables = jest.fn(() => searchables);
             const searchEngine = new SearchEngine(
                 { threshold: 0.6, automaticRescanIntervalInSeconds: 0, automaticRescanEnabled: false },
-                [new DummySearchPlugin(tempFolderPath, onGetAllSearchables)],
-                logger
+                [searchPlugin.object()],
+                logger.object()
             );
+
             await searchEngine.initialize();
             const actual = searchEngine.search("srch rslt");
+
             expect(actual).toEqual(searchables.map((searchable) => searchable.toSearchResultItem()));
-            expect(onGetAllSearchables.mock.calls.length).toBe(1);
+            verifyLoggerLoggedInfo("Starting rescan", Times.Once());
         });
 
         it("should not supporty fuzzy search if threshold is 0", async () => {
-            const onGetAllSearchables = jest.fn(() => searchables);
             const searchEngine = new SearchEngine(
                 { threshold: 0, automaticRescanIntervalInSeconds: 0, automaticRescanEnabled: false },
-                [new DummySearchPlugin(tempFolderPath, onGetAllSearchables)],
-                logger
+                [searchPlugin.object()],
+                logger.object()
             );
+
             await searchEngine.initialize();
             const actual = searchEngine.search("srch rslt");
+
             expect(actual).toEqual([]);
-            expect(onGetAllSearchables.mock.calls.length).toBe(1);
+            verifyLoggerLoggedInfo("Starting rescan", Times.Once());
         });
     });
 });
