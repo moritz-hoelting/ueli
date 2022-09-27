da01eafc71977a3c49f09dfecf3a53a34701a54d
Oliver Schwendener <oliver.schwendener@hotmail.com>
2022-09-27 23:22:12 +0200

    Use object mocks instead of dummy classes

--- src/main/Core/LocationOpeningService.test.ts
+++ src/main/Core/LocationOpeningService.test.ts
@@ -1,35 +1,51 @@
+import { IMock, Mock, Times } from "moq.ts";
+import { SearchResultItem } from "../../common/SearchResult/SearchResultItem";
 import { SearchResultItemDummy } from "../../common/SearchResult/SearchResultItemDummy";
-import { LocationOpenerDummy } from "../LocationOpeners/LocationOpenerDummy";
+import { LocationOpener } from "../LocationOpeners/LocationOpener";
 import { LocationOpeningService } from "./LocationOpeningService";
 
 describe(LocationOpeningService, () => {
+    const dummyLocationOpenerId = "LocationOpenerDummy";
+    const searchResultItemDummy = SearchResultItemDummy.withLocationOpenerId(dummyLocationOpenerId);
+    let locationOpener: IMock<LocationOpener>;
+
+    beforeEach(() => {
+        locationOpener = new Mock<LocationOpener>()
+            .setup((instance) => instance.locationOpenerId)
+            .returns(dummyLocationOpenerId)
+            .setup((instance) => instance.openLocation(searchResultItemDummy))
+            .returns(Promise.resolve());
+    });
+
     describe(LocationOpeningService.prototype.openLocation, () => {
         it("should succeed if there is an corresponding location opener", async () => {
-            const locationOpenerDummy = new LocationOpenerDummy();
-            const locationOpeningService = new LocationOpeningService([locationOpenerDummy]);
-            await locationOpeningService.openLocation(
-                SearchResultItemDummy.withLocationOpenerId(locationOpenerDummy.locationOpenerId)
-            );
+            const locationOpeningService = new LocationOpeningService([locationOpener.object()]);
+            await locationOpeningService.openLocation(searchResultItemDummy);
+
+            locationOpener.verify((instance) => instance.openLocation(searchResultItemDummy), Times.Once());
         });
 
         it("should fail if the corresponding location opener fails", async () => {
-            const locationOpenerDummy = new LocationOpenerDummy("LocationOpenerDummy", false);
-            const locationOpeningService = new LocationOpeningService([locationOpenerDummy]);
+            expect.assertions(1);
+
+            locationOpener
+                .setup((instance) => instance.openLocation(searchResultItemDummy))
+                .returns(Promise.reject("Failed"));
+
             try {
-                await locationOpeningService.openLocation(
-                    SearchResultItemDummy.withLocationOpenerId(locationOpenerDummy.locationOpenerId)
-                );
+                await new LocationOpeningService([locationOpener.object()]).openLocation(searchResultItemDummy);
             } catch (error) {
                 expect(error).toBe("Failed");
             }
         });
 
         it("should fail if there is no corresponding location opener", async () => {
-            const locationOpeningService = new LocationOpeningService([new LocationOpenerDummy()]);
+            expect.assertions(1);
+
             try {
-                await locationOpeningService.openLocation(
-                    SearchResultItemDummy.withLocationOpenerId("Some other opener id")
-                );
+                await new LocationOpeningService([locationOpener.object()]).openLocation(<SearchResultItem>{
+                    locationOpenerId: "Some other opener id",
+                });
             } catch (error) {
                 expect(error).toBe(
                     `Unable to open location for "Some other opener id". Reason: no location opener found.`
